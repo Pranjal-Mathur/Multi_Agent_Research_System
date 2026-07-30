9d3230a2992e8b4e6b6dc1df6f31be130a965982
Pranjal <mathurpranjal20@gmail.com>
2026-07-30 20:47:59 +0530

    Fix pipeline

--- agents.py
+++ agents.py
@@ -1,5 +1,5 @@
 from langchain.agents import create_agent
-from langchain_groq import ChatGroq
+# from langchain_groq import ChatGroq
 from langchain_mistralai import ChatMistralAI
 from langchain_core.prompts import ChatPromptTemplate
 from langchain_core.output_parsers import StrOutputParser
